2a3dafd0bf8faa893e804bc1ea7e8a3769da37ff
Jakria sultana jui <85211928+Jakriasultanajui@users.noreply.github.com>
2022-03-11 04:36:50 +0600

    Add files via upload

---
+++ Lab 4/PublicHome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php include('Header.php');?>
+<table>
+	<h1 style="text-align:center;">Wellcome to Xcompany</h1>
+<tr>	
+<td style="padding: 0%">
+</td>
+<td style="padding: 0%">
+</td>
+<td style="padding: 0%">
+</td>
+</tr>
+</table>
+
+
+<?php include('Footer.php');?>
+
+</body>
+</html>
